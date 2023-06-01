15f8808ab29e92ba40d0b1c7ea9c25f48ec36d7c
naachopedraza1 <nachi_choco@live.com.ar>
2023-06-01 04:09:12 -0300

    stats add

--- components/StatsGroup.tsx
+++ components/StatsGroup.tsx
@@ -6,10 +6,10 @@ import { EmojiEvents, HeadsetMic, Person, PersonalVideo } from "@mui/icons-mater
 
 
 const stats = [
-    { title: "Clientes satisfechos", value: 80, icon: <Person /> },
-    { title: "Páginas web realizadas", value: 100, icon: <PersonalVideo /> },
-    { title: "Consultas de soporte", value: 250, icon: <HeadsetMic /> },
-    { title: "Casos de éxito", value: 70, icon: <EmojiEvents /> }
+    { title: "Clientes satisfechos", value: 20, icon: <Person /> },
+    { title: "Páginas web realizadas", value: 30, icon: <PersonalVideo /> },
+    { title: "Consultas de soporte", value: 80, icon: <HeadsetMic /> },
+    { title: "Casos de éxito", value: 15, icon: <EmojiEvents /> }
 ]
 
 export const StatsGroup = () => {
